eeef461387284e7c20c33b7ab9d7a22a30472e7d
paulino <pjcoderias@gmail.com>
2016-07-09 17:06:40 +0800

    Third Commit

--- djnotes/notes/views.py
+++ djnotes/notes/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from .models import Note
+
+
 # Create your views here.
 class Index(View):
 	template_name = 'notes/index.html'
 
 	def get_context_data(self):
-		context = {}
+		context = {
+			'notes': Note.objects.order_by('-id'),
+		}
 		return context
 
 	def get(self, request, *args, **kwargs):
